0598ebd6b9b27fd7ac7e14f476d1362133a12ca0
vandersonsilvapaim <33205433+vandersonsilvapaim@users.noreply.github.com>
2017-11-12 11:36:07 -0200

    Update Alterando tabelas.sql

--- Desvendando banco de dados relacionais com Mysql/Alterando tabelas.sql
+++ Desvendando banco de dados relacionais com Mysql/Alterando tabelas.sql
@@ -1,1 +1,20 @@
 
+CREATE SCHEMA my_schema ;
+
+CREATE TABLE my_schema.tabela_1 (a INTEGER, b CHAR(5));
+
+ALTER TABLE my_schema.tabela_1 rename my_schema.pais;
+
+ALTER TABLE my_schema.pais CHANGE COLUMN a id INTEGER ;
+
+ALTER TABLE my_schema.pais MODIFY b VARCHAR(100);
+
+ALTER TABLE my_schema.pais MODIFY id INTEGER NOT NULL, CHANGE b nome_ptbr VARCHAR(100) NOT NULL;
+
+ALTER TABLE my_schema.pais ADD nome_eng VARCHAR(100) NOT NULL;
+
+ALTER TABLE my_schema.pais ADD nome_rus VARCHAR(100) NOT NULL AFTER nome_eng , ADD continente VARCHAR(100) NULL, ADD capital VARCHAR(100) NULL;
+ 
+ALTER TABLE my_schema.pais DROP COLUMN nome_rus;
+
+DESCRIBE my_schema.pais; 
